cc815b28420b7395884064ea3f3793ca3ef8e01c
KailerS <KailerS@users.noreply.github.com>
2019-10-14 21:55:28 -0600

    All the basics are working needs refining but it works

--- app/data/assets/clicks.js
+++ app/data/assets/clicks.js
@@ -1,4 +1,30 @@
 $("#submit").on("click", function (event) {
     event.preventDefault();
     
+    var newCharacter = {
+        name: $("#name").val().trim(),
+        photo: $("#image").val().trim(),
+        scores: [
+            $("#q1").val(),
+            $("#q2").val(),
+            $("#q3").val(),
+            $("#q4").val(),
+            $("#q5").val(),
+            $("#q6").val(),
+            $("#q7").val(),
+            $("#q8").val(),
+            $("#q9").val(),
+            $("#q10").val()
+        ]
+    };
+
+    console.log(newCharacter);
+
+    $.post("api/characters", newCharacter, function (data){
+        if (data){
+            console.log(data);
+            $(".modal-body").append(`<h1>${data.name}`);
+            $(".modal-body").append(`<img src=${data.photo}>`);
+        }
+    })
 })
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,4 +8,39 @@ module.exports = function (app){
     app.get("/api/characters", function(req, res) {
         res.json(characters);
     });
+
+    app.post("/api/characters", function (req, res){
+        console.log(req.body);
+        var score1 = 0;
+        var score2 = 0;
+        var score3 = 0;
+        for (i=0; i < req.body.scores.length; i++){
+            score1 += (Math.abs(parseInt(req.body.scores[i]) - parseInt(characters[0].scores[i])));
+        };
+        for (i=0; i < req.body.scores.length; i++){
+            score2 += (Math.abs(parseInt(req.body.scores[i]) - parseInt(characters[1].scores[i])));
+        };
+        for (i=0; i < req.body.scores.length; i++){
+            score3 += (Math.abs(parseInt(req.body.scores[i]) - parseInt(characters[2].scores[i])));
+        };
+
+        if(score1 < score2 && score1 < score3){
+            return res.json(characters[0]);
+        } else if (score2 < score1 && score2 < score3){
+            return res.json(characters[1]);
+        }else if (score3 < score1 && score3 < score2){
+            return res.json(characters[2])
+        } else{
+            return res.send("No characters matched");
+        };
+
+        console.log(score1);
+        console.log(score2);
+        console.log(score3);
+
+
+        res.end();
+
+
+    });
 };
