421b2b62948b5e08270c1162f8e456dec4ca88ca
KailerS <KailerS@users.noreply.github.com>
2019-10-14 19:27:02 -0600

    Added all questions and characters into the array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,50 @@
-var characters = [];
+var characters = [
+    {
+        name: "Taco",
+        photo: "https://media.giphy.com/media/XGEOuWbzxBBle/giphy.gif",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ],
+    },{
+        name: "Pete",
+        photo: "https://media.giphy.com/media/138KntZJsPdPxK/giphy.gif",
+        scores: [
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0            
+        ],
+    },{
+        name: "Ruxin",
+        photo: "https://media.giphy.com/media/10Gp0wzMg4LB0A/giphy.gif",
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3                        
+        ],
+    }
+];
 
 module.exports = characters;
