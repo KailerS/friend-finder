1dca814d3e18721397b373a52e67bfd59cea2e72
KailerS <KailerS@users.noreply.github.com>
2019-10-14 19:56:08 -0600

    Added the html paths and the route to api/ characters

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,11 @@
 var characters = require("../data/friends");
+
+
+
+
+module.exports = function (app){
+
+    app.get("/api/characters", function(req, res) {
+        res.json(characters);
+    });
+};
--- server.js
+++ server.js
@@ -3,3 +3,12 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static("app"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
